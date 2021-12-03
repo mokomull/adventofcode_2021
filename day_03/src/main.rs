450588b9c344bbfacb887c065a2cee9b1b28fde8
Matt Mullins <mmullins@mmlx.us>
2021-12-02 23:39:59 -0800

    day 03: filter O2 and CO2 based on given criteria
    This is slightly awkward logic, but at least the data doesn't make me
    loop through the bits more than once.

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -36,4 +36,37 @@ fn main() {
 
     let mut oxygen_candidates = input.clone();
     let mut co2_candidates = input.clone();
+
+    for i in 0..oxygen_candidates[0].len() {
+        if oxygen_candidates.len() == 1 {
+            break;
+        }
+
+        match compare_ones_to_zeroes(&oxygen_candidates, i) {
+            Ordering::Greater | Ordering::Equal => {
+                oxygen_candidates.retain(|&s| s[i] == b'1');
+            }
+            Ordering::Less => oxygen_candidates.retain(|&s| s[i] == b'0'),
+        }
+    }
+    assert!(oxygen_candidates.len() == 1);
+
+    for i in 0..co2_candidates[0].len() {
+        if co2_candidates.len() == 1 {
+            break;
+        }
+
+        match compare_ones_to_zeroes(&co2_candidates, i) {
+            Ordering::Greater | Ordering::Equal => co2_candidates.retain(|&s| s[i] == b'0'),
+            Ordering::Less => co2_candidates.retain(|&s| s[i] == b'1'),
+        }
+    }
+    assert!(co2_candidates.len() == 1);
+
+    let oxygen = u64::from_str_radix(std::str::from_utf8(oxygen_candidates[0]).unwrap(), 2)
+        .expect("can't parse oxygen");
+    let co2 = u64::from_str_radix(std::str::from_utf8(co2_candidates[0]).unwrap(), 2)
+        .expect("can't parse co2");
+    let part2 = oxygen * co2;
+    dbg!(part2);
 }
