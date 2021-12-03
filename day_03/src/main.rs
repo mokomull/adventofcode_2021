93067fd0b8478c8965bb78519d0a80b98337100f
Matt Mullins <mmullins@mmlx.us>
2021-12-02 23:32:15 -0800

    day 03: factor out compare_ones_to_zeroes

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,20 +1,30 @@
+use std::cmp::Ordering;
+
 use prelude::*;
+
+fn compare_ones_to_zeroes<'a>(input: &[&'a [u8]], bit_position: usize) -> Ordering {
+    let ones = input.iter().filter(|&s| s[bit_position] == b'1').count();
+    let zeroes = input.len() - ones;
+
+    ones.cmp(&zeroes)
+}
 
 fn main() {
     let input = read_lines_from_file("inputs/day_03.txt").collect::<Vec<_>>();
+    let input: Vec<_> = input.iter().map(|s| s.as_bytes()).collect();
 
     let mut gamma = String::new();
     let mut epsilon = String::new();
-    for i in 0..input[0].as_bytes().len() {
-        let ones = input.iter().filter(|&s| s.as_bytes()[i] == b'1').count();
-        let zeroes = input.len() - ones;
-
-        if ones > zeroes {
-            gamma.push('1');
-            epsilon.push('0');
-        } else {
-            gamma.push('0');
-            epsilon.push('1');
+    for i in 0..input[0].len() {
+        match compare_ones_to_zeroes(&input, i) {
+            Ordering::Greater => {
+                gamma.push('1');
+                epsilon.push('0');
+            }
+            Ordering::Equal | Ordering::Less => {
+                gamma.push('0');
+                epsilon.push('1');
+            }
         }
     }
 
@@ -23,4 +33,7 @@ fn main() {
 
     let part1 = gamma * epsilon;
     dbg!(part1);
+
+    let mut oxygen_candidates = input.clone();
+    let mut co2_candidates = input.clone();
 }
