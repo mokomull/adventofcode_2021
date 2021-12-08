57c83a6f162b76758a88ea8de1c7dfe74e304221
Matt Mullins <mmullins@mmlx.us>
2021-12-07 23:24:33 -0800

    day 08: narrow possibilities by unique patterns
    This won't work, for the reason in the comment.

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -30,6 +30,46 @@ fn do_main(input: &str) {
         .flatten()
         .count();
     dbg!(part1);
+
+    let part2 = input
+        .iter()
+        .map(|(observations, output)| {
+            // the problem statement says the signals are
+            //     a
+            //   b   c
+            //     d
+            //   e   f
+            //     g
+
+            // we can't just iteratively intersect the set of possibilities, because *all* of the
+            // unique-count-of-segments have both C and F lit.
+
+            // signals a-g correspond to indexes 0 through 6 in the following array of possibilities:
+            // created with the trick in https://stackoverflow.com/a/69756635 to initialize with
+            // something that's not Copy (i.e. a HashSet)
+            let mut possibilities = [(); 7].map(|()| {
+                ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+                    .into_iter()
+                    .collect::<HashSet<_>>()
+            });
+
+            for observation in observations {
+                let potential_segments: &[char] = match observation.len() {
+                    2 => &['c', 'f'],
+                    4 => &['b', 'c', 'd', 'f'],
+                    3 => &['a', 'c', 'f'],
+                    _ => continue,
+                };
+
+                for c in observation.chars() {
+                    possibilities[c as usize - 'a' as usize]
+                        .retain(|c| potential_segments.contains(c));
+                }
+            }
+
+            dbg!(possibilities);
+        })
+        .collect_vec();
 }
 
 fn main() {
