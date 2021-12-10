53c74f8393c78ce37cbbd40d41dcd93851898661
Matt Mullins <mmullins@mmlx.us>
2021-12-09 20:45:59 -0800

    day 09: count lowest points in the caves

---
+++ day_09/src/main.rs
@@ -0,0 +1,35 @@
+use std::convert::identity;
+
+use prelude::*;
+
+fn do_main(input: &str) {
+    let input = read_lines_from_file(input)
+        .map(|line| line.bytes().map(|cell| cell - b'0').collect_vec())
+        .collect_vec();
+
+    let mut part1 = 0u64;
+    for i in 0..input.len() {
+        for j in 0..input[0].len() {
+            let adjacent = [
+                input.get(i).and_then(|row| row.get(j.wrapping_sub(1))),
+                input.get(i).and_then(|row| row.get(j + 1)),
+                input.get(i.wrapping_sub(1)).and_then(|row| row.get(j)),
+                input.get(i + 1).and_then(|row| row.get(j)),
+            ];
+
+            if adjacent
+                .into_iter()
+                .filter_map(identity)
+                .all(|&other| other > input[i][j])
+            {
+                part1 += 1;
+            }
+        }
+    }
+
+    dbg!(part1);
+}
+
+fn main() {
+    do_main("inputs/day_09.txt");
+}
