b6da99e2e8929735567b2269dc1d886afa985dbb
Matt Mullins <mmullins@mmlx.us>
2021-12-09 21:11:58 -0800

    day 10: pop incomplete lines off

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -4,8 +4,11 @@ fn do_main(input: &str) {
     let input = read_lines_from_file(input).collect_vec();
 
     let mut part1 = 0u64;
+    let mut part2 = 0u64;
     for line in &input {
         let mut state = vec![];
+        let mut error = false;
+
         for c in line.chars() {
             match c {
                 '[' | '(' | '<' | '{' => {
@@ -21,12 +24,27 @@ fn do_main(input: &str) {
                         '>' if opener != '<' => part1 += 25137,
                         _ => continue,
                     }
+                    error = true;
                     break;
+                }
+            }
+        }
+
+        if !error {
+            for c in state.into_iter().rev() {
+                part2 *= 5;
+                match c {
+                    '(' => part2 += 1,
+                    '[' => part2 += 2,
+                    '{' => part2 += 3,
+                    '<' => part2 += 4,
+                    _ => panic!("oops {}", c),
                 }
             }
         }
     }
     dbg!(part1);
+    dbg!(part2);
 }
 
 fn main() {
