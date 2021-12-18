ef8e3b51f207d0f8366782b215f27ff517024934
Matt Mullins <mmullins@mmlx.us>
2021-12-18 15:37:14 -0800

    day 18: mostly implement exploding
    This kinda works, but adds one too many things to the right?
    
    Frankly, I can't believe this passed the borrow-checker.

--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 enum Snailfish {
     Normal(u64),
@@ -6,7 +8,56 @@ enum Snailfish {
 
 impl Snailfish {
     fn exploded(&mut self) -> bool {
-        unimplemented!()
+        let mut left = None;
+        let mut add_to_right = None;
+
+        // Visit the nodes in-order, keeping track of the previously seen regular number, because
+        // that is what the left element explodes into.
+        let mut to_visit: VecDeque<_> = [(self, 0)].into();
+        while !to_visit.is_empty() {
+            let (this, depth) = to_visit.pop_front().unwrap();
+
+            match this {
+                // if add_to_right has been set, then we have found an exploding snailfish number.
+                // after we update `this`, we're completely done searching.
+                Snailfish::Normal(ref mut inner) if add_to_right.is_some() => {
+                    *inner += add_to_right.unwrap();
+                    break;
+                }
+
+                Snailfish::Normal(_) => {
+                    left = Some(this);
+                }
+
+                Snailfish::Pair(a, b) if depth >= 4 => {
+                    // the flavortext says that exploding pairs will only have regular numbers as
+                    // children.
+                    if let Snailfish::Normal(a) = **a {
+                        if let Snailfish::Normal(b) = **b {
+                            if let Some(&mut Snailfish::Normal(ref mut left)) = left {
+                                *left += a;
+                            }
+                            add_to_right = Some(b);
+                            *this = Snailfish::Normal(0);
+                            continue;
+                        }
+                    }
+                    panic!(
+                        "Found a pair of not-regular numbers that would explode: {:?}",
+                        this
+                    );
+                }
+
+                Snailfish::Pair(ref mut a, ref mut b) => {
+                    to_visit.push_back((a, depth + 1));
+                    to_visit.push_back((b, depth + 1));
+                }
+            }
+        }
+
+        // if and only if add_to_right was set, we experienced an explosion.  Use that as our
+        // sentinel.
+        add_to_right.is_some()
     }
 }
 
