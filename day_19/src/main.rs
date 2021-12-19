1380a4551d67d577b41a2174688f8cb44c540469
Matt Mullins <mmullins@mmlx.us>
2021-12-18 23:55:32 -0800

    day 19: find path in graph of pairs of nodes
    This takes longer to compute since it can no longer bail early when a
    scanner has already been compared to other scanners, but it finds a lot
    more pairs-of-scanners-sharing-points.  And it finds a path to scanner
    0, which I have declared is the canonical reference frame.

--- day_19/src/main.rs
+++ day_19/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use prelude::*;
 
 #[derive(Clone, Copy, Debug)]
@@ -88,18 +90,13 @@ fn do_main(input: &str) {
 
     // define scanner 0 to be centered at 0, 0, 0, and right-side-up.
 
-    // Maps scanner id -> (previous scanner id, transform to get from previous to this one, offset
+    // Maps (scanner id, previous scanner id) -> (transform to get from previous to this one, offset
     // to get from previous to this one)
-    let mut locations: HashMap<usize, (usize, Vec<Transform>, [i32; 3])> =
-        [(0, (0, vec![], [0, 0, 0]))].into();
+    let mut locations: HashMap<(usize, usize), (Vec<Transform>, [i32; 3])> = HashMap::new();
 
     'scanner: for permutation in scanners.iter().enumerate().permutations(2) {
         let (origin_id, origin) = permutation[0];
         let (this_id, this_scanner) = permutation[1];
-
-        if locations.contains_key(&this_id) {
-            continue;
-        }
 
         // try every possible facing direction and "up" direction
         for orientation in [Pos(X), Pos(Y), Pos(Z), Neg(X), Neg(Y), Neg(Z)] {
@@ -138,7 +135,7 @@ fn do_main(input: &str) {
                             .count();
 
                         if matching_beacons >= 12 {
-                            locations.insert(this_id, (origin_id, transforms, offset));
+                            locations.insert((this_id, origin_id), (transforms, offset));
                             continue 'scanner;
                         }
                     }
@@ -146,18 +143,36 @@ fn do_main(input: &str) {
             }
         }
     }
-    // double-check that we did indeed find an orientation/offset for each scanner
-    assert_eq!(locations.len(), scanners.len());
+
+    // map scanner_id to previous scanner ID on a path toward 0
+    let mut predecessors: HashMap<usize, usize> = [(0, 0)].into();
+    // Breadth-first search for easy graph traversal.
+    let mut to_visit: VecDeque<usize> = [0].into();
+    while !to_visit.is_empty() {
+        let this = to_visit.pop_front().unwrap();
+        for (origin, target) in locations.keys() {
+            if *origin != this {
+                continue;
+            }
+            if predecessors.contains_key(target) {
+                continue;
+            }
+            predecessors.insert(*target, this);
+            to_visit.push_back(*target);
+        }
+    }
 
     let mut beacons = HashSet::new();
     for (id, scanner) in scanners.iter().enumerate() {
         for point in scanner {
             let mut translated = *point;
-            let mut translate_id = id;
+            let mut current_reference_id = id;
             // translate the point to the reference scanner, until we reach 0
-            while translate_id != 0 {
-                let (next_id, transform, offset) = locations.get(&translate_id).unwrap();
+            while current_reference_id != 0 {
+                let next_id = predecessors[&current_reference_id];
+                let (transform, offset) = locations.get(&(current_reference_id, next_id)).unwrap();
                 translated = Transform::apply_offset(transform, translated, *offset);
+                current_reference_id = next_id;
             }
             beacons.insert(translated);
         }
