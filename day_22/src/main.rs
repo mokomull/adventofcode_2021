7978a8f7727c495558cbede09dc3a43d9725c565
Matt Mullins <mmullins@mmlx.us>
2021-12-21 22:49:08 -0800

    day 22: blindly turn cubes on and off and count

--- day_22/src/main.rs
+++ day_22/src/main.rs
@@ -5,8 +5,8 @@ use prelude::*;
 type Axis = RangeInclusive<i32>;
 
 enum Instruction {
-    On(Axis, Axis, Axis),
-    Off(Axis, Axis, Axis),
+    On,
+    Off,
 }
 use Instruction::*;
 
@@ -19,25 +19,44 @@ fn do_main(input: &str) {
         .map(|line| {
             let groups = regex.captures(&line).unwrap();
             let mut groups = groups.iter();
-            let on_off = groups.nth(1).unwrap().unwrap();
+            let on_off = match groups.nth(1).unwrap().unwrap().as_str() {
+                "on" => On,
+                "off" => Off,
+                x => panic!("what is {}", x),
+            };
             let numbers = groups
                 .map(|m| m.unwrap().as_str().parse::<i32>().unwrap())
                 .collect_vec();
-            match on_off.as_str() {
-                "on" => On(
-                    numbers[0]..=numbers[1],
-                    numbers[2]..=numbers[3],
-                    numbers[4]..=numbers[5],
-                ),
-                "off" => Off(
-                    numbers[0]..=numbers[1],
-                    numbers[2]..=numbers[3],
-                    numbers[4]..=numbers[5],
-                ),
-                x => panic!("what is {}", x),
-            }
+            (
+                on_off,
+                numbers[0]..=numbers[1],
+                numbers[2]..=numbers[3],
+                numbers[4]..=numbers[5],
+            )
         })
         .collect_vec();
+
+    let mut cubes = HashSet::new();
+
+    for i in &input {
+        for x in (*i.1.start()).max(-50)..=(*i.1.end()).min(50) {
+            for y in (*i.2.start()).max(-50)..=(*i.2.end()).min(50) {
+                for z in (*i.3.start()).max(-50)..=(*i.3.end()).min(50) {
+                    match i.0 {
+                        On => {
+                            cubes.insert((x, y, z));
+                        }
+                        Off => {
+                            cubes.remove(&(x, y, z));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let part1 = cubes.len();
+    dbg!(part1);
 }
 
 fn main() {
